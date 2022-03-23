276b602284c8b8c1ede950d018951a605864a4e4
Amélie Roynette <amelieroynette@Amelies-MacBook-Air.local>
2022-03-23 11:31:08 +0100

    home page/nav/buttons

--- bubble_my_tea/routes/web.php
+++ bubble_my_tea/routes/web.php
@@ -21,4 +21,8 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+Route::get('/profil', function () {
+    return view('profil');
+});
+
 require __DIR__.'/auth.php';
