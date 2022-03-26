dff68b8e2fc1915ae37cb2bcdd23f64f6f66688f
Jean-Antoine <maro_j@etna-alternance.net>
2022-03-26 12:14:51 +0100

    css profil

--- bubble_my_tea/resources/views/profil.blade.php
+++ bubble_my_tea/resources/views/profil.blade.php
@@ -3,9 +3,9 @@
 @section('content')
    <h2> Mes commandes</h2>
    <br>
-
+   <div class="order-container">
    @foreach($orders as $order)    
-
+      <div class="cmd">
             <p> Numéro de la commande : {{$order->id}} </p>
 
             @foreach ($order->orderProducts as $op)
@@ -18,8 +18,27 @@
             
             prix de la commande = {{ $order->price }} euros <br>
             <br>
-
+      </div>
    @endforeach
+   </div>
+
+   <style>
+      h2 {
+         text-align: center;
+         font-weight: bold;
+         margin-top: 2rem;
+      }
+      .cmd {
+         margin: 1rem;
+         border-radius: 8px;
+         background-color: rgb(254 226 226);
+         padding: 1rem;
+      }
+      .order-container {
+         display:flex;
+          flex-flow: row wrap;
+      }
+   </style>
 
 @endsection
 
